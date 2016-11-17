d0e1d63d9a62e12272a5e72425a9c3f81c0a6f73
Gantyuk Vyacheslav <Vgantyuk2@gmail.com>
2016-11-18 00:17:13 +0200

    sechond trai

--- Lab_1.py
+++ Lab_1.py
@@ -5,10 +5,12 @@ a = float(input('a = '))
 e = float(input('e = '))
 suma = 1.0
 k = 1
+#fdghjkl;
 precision = 1.0
 while precision > e:
     precision = math.pow(math.log(a + x), 2 * k) / (math.pow(2, k) + math.factorial(k))
     suma +=precision 
     k = k + 1
+	
 print("Сума=" + str(suma))
 print("Кількість врахованих доданків:" + str(k))
