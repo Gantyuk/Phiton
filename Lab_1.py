7670b859fc9bb6860c67e7e6170397eb7c735135
Gantyuk Vyacheslav <Vgantyuk2@gmail.com>
2016-11-18 00:05:49 +0200

    firs trai

---
+++ Lab_1.py
@@ -0,0 +1,14 @@
+import math
+# щось поміняв 
+x = float(input('x = '))
+a = float(input('a = '))
+e = float(input('e = '))
+suma = 1.0
+k = 1
+precision = 1.0
+while precision > e:
+    precision = math.pow(math.log(a + x), 2 * k) / (math.pow(2, k) + math.factorial(k))
+    suma +=precision 
+    k = k + 1
+print("Сума=" + str(suma))
+print("Кількість врахованих доданків:" + str(k))
