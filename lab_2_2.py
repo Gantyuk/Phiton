7cc42becc7db3000c95e233bb0ff19477269cd40
Gantyuk Vyacheslav <Vgantyuk2@gmail.com>
2016-11-19 18:35:47 +0200

    Second_lab

---
+++ lab_2_2.py
@@ -0,0 +1,26 @@
+import math
+
+def a(k):
+        if k == 1:
+            return 1
+        else:
+            return (3 * b(k - 1) + 2 * a(k - 1))
+        
+def b(k):
+        if k == 1:
+            return 1
+        else:
+            return (a(k - 1) * a(k - 1) + b(k - 1))
+        
+def sum_expres(n):
+    if n == 0:
+        return 0
+    k = 1
+    suma = 0
+    while k < n:
+        suma = suma + ((2**k) / (1 + a(k) + b(k)))
+        k = k + 1
+    return suma    
+
+n = int(input('n = '))
+print("Сума="+str(sum_expres(n)))
