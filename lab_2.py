cd2ba0f9e3d4aca2df2ba11e064b5d39adda3b8b
Gantyuk Vyacheslav <Vgantyuk2@gmail.com>
2016-11-19 17:24:01 +0200

    lab2

---
+++ lab_2.py
@@ -0,0 +1,27 @@
+import math
+from random import randrange as rnd, choice
+
+def scalar_product(vector1,vector2,count1):
+    suma=0
+    for i in range(count1):
+         suma+=vector1[i]*vector2[i]
+    return suma
+
+count = int(input('n = '))
+matrix = []
+for r in range(count):
+    matrix.append([])
+    for c in range(count):
+        matrix[r].append(rnd(10,100))
+print("X:")
+for r in range(count):
+    print(matrix[r])
+
+matrixY= []
+for r in range(count):
+    matrixY.append([])
+    for c in range(count):
+        matrixY[r].append(scalar_product(matrix[r],matrix[c],count))
+print("Y:")     
+for r in range(count):
+    print(matrixY[r])
